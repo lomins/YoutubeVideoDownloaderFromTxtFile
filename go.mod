1ff650e900907652386a624ab02436bfc27b8cd3
lomins <lominsp54@gmail.com>
2023-09-17 16:08:48 +0300

    Init commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module example.org/youtubevideodownloader
+
+go 1.21.0
+
+require github.com/kkdai/youtube/v2 v2.8.4
+
+require (
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
+	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/dop251/goja v0.0.0-20230812105242-81d76064690d // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/google/pprof v0.0.0-20230811205829-9131a7e9cc17 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/vbauerster/mpb/v5 v5.4.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/kkdai/youtube/v2"
+)
+
+func main() {
+	var videosIDs []string
+
+	file, err := os.Open("videosIDs.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		videosIDs = append(videosIDs, scanner.Text())
+	}
+
+	client := youtube.Client{}
+
+	defer fmt.Scanln()
+	for _, videoID := range videosIDs {
+
+		video, err := client.GetVideo(videoID)
+		if err != nil {
+			panic(err)
+		}
+
+		formats := video.Formats.Quality("hd1080").WithAudioChannels()
+		if formats == nil {
+			formats = video.Formats.WithAudioChannels()
+		}
+		//formats := video.Formats.WithAudioChannels() // only get videos with audio
+
+		stream, _, err := client.GetStream(video, &formats[0])
+
+		if err != nil {
+			panic(err)
+		}
+
+		videoTitle := video.Title
+		videoTitle = strings.Replace(videoTitle, "?", "", -1)
+		videoTitle = strings.Replace(videoTitle, "!", "", -1)
+
+		file, err = os.Create(videoTitle + ".mp4")
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		_, err = io.Copy(file, stream)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Succefull download: " + videoTitle)
+	}
+	fmt.Println("Нажмите Enter для выхода...")
+}
